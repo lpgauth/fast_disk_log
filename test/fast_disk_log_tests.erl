18a45a9a70b2875edf16131dd7b1d7e9b573d1f7
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-05-29 12:24:01 -0400

    Add tests

--- test/fast_disk_log_tests.erl
+++ test/fast_disk_log_tests.erl
@@ -14,11 +14,15 @@ fast_disk_logger_test_() ->
         fun () -> setup() end,
         fun (_) -> cleanup() end,
     [
-        ?T(test_open)
+        ?T(test_open_close)
     ]}.
 
 %% tests
-test_open() ->
+test_open_close() ->
+    {error, no_such_log} = fast_disk_log:close(?LOGGER_NAME),
+    ok = fast_disk_log:open(?LOGGER_NAME, ?LOGGER_PATH),
+    {error, name_already_open} = fast_disk_log:open(?LOGGER_NAME, ?LOGGER_PATH),
+    ok = fast_disk_log:close(?LOGGER_NAME),
     ok = fast_disk_log:open(?LOGGER_NAME, ?LOGGER_PATH).
 
 %% utils
@@ -30,11 +34,19 @@ cleanup() ->
 random() ->
     crypto:rand_bytes(24).
 
-receive_loop(0) -> [];
-receive_loop(N) ->
-    receive
-        {response, X} ->
-            [X | receive_loop(N - 1)]
+read_loop(N) ->
+    {ok, File} = file:open(?LOGGER_PATH, [read]),
+    read_loop(N, File).
+
+read_loop(0, _File) ->
+    [];
+read_loop(N, File) ->
+    case file:read_line(File) of
+        {ok, Line} ->
+            [Line | read_loop(N - 1)];
+        eof ->
+            timer:sleep(500),
+            read_loop(N)
     end.
 
 setup() ->
